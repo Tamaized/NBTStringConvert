a37db04478f60d7aa778919dada87ab95ce857f4
Tamaized <ryanolt123@gmail.com>
2017-10-21 21:10:22 -0700

    cleanup

--- src/Tamaized/NBTStringConvert/NBTStringConvert.java
+++ src/Tamaized/NBTStringConvert/NBTStringConvert.java
@@ -1,8 +1,14 @@
 package Tamaized.NBTStringConvert;
 
+import org.jnbt.CompoundTag;
+import org.jnbt.ListTag;
+import org.jnbt.NBTInputStream;
+import org.jnbt.NBTOutputStream;
+import org.jnbt.StringTag;
+import org.jnbt.Tag;
+
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -12,94 +18,63 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.jnbt.CompoundTag;
-import org.jnbt.ListTag;
-import org.jnbt.NBTInputStream;
-import org.jnbt.NBTOutputStream;
-import org.jnbt.StringTag;
-import org.jnbt.Tag;
-
 public class NBTStringConvert {
 
-	static final List<File> files = new ArrayList<File>();
+	private static final List<File> files = new ArrayList<>();
 
 	// We want arg 1: folder path, arg 2: String Tag's Name, arg 3: string to replace, arg 4: string to be placed
-	public static void main(String[] args) throws FileNotFoundException, IOException {
-
+	public static void main(String[] args) throws IOException {
 		if (args.length > 3) {
-
 			String folder = args[0];
 			String name = args[1];
 			String replace = args[2];
 			String set = args[3];
-
 			scanFiles(folder);
 			replaceFileContents(name, replace, set);
-
-		} else {
+		} else
 			System.out.println("Not Enough Args");
-		}
-
 	}
 
-	static void scanFiles(String path) {
-
+	private static void scanFiles(String path) {
 		File folder = new File(path);
-
 		if (folder.isDirectory()) {
-
-			for (File file : folder.listFiles()) {
-
-				if (file.getName().toLowerCase().endsWith(".nbt")) {
-
-					System.out.println("Found File: " + file.getName());
-					files.add(file);
-
-				}
-
-			}
-
-		} else {
+			File[] list = folder.listFiles();
+			if (list != null)
+				for (File file : list)
+					if (file.getName().toLowerCase().endsWith(".nbt")) {
+						System.out.println("Found File: " + file.getName());
+						files.add(file);
+					}
+		} else
 			System.out.println("Path was not a folder");
-		}
-
 	}
 
-	static void replaceFileContents(String name, String replace, String set) throws FileNotFoundException, IOException {
+	private static void replaceFileContents(String name, String replace, String set) throws IOException {
 
 		for (File file : files) {
-
 			System.out.println("Scanning: " + file.getName());
 			Tag tag;
 			NBTInputStream stream = new NBTInputStream(new FileInputStream(file));
-			{
-				tag = stream.readTag();
-				if (tag instanceof StringTag) {
-					StringTag stringTag = (StringTag) tag;
-					if (stringTag.getName().equals(name) && stringTag.getValue().equals(replace)) {
-						System.out.println("(StringTag) Replacing: " + replace + " with: " + set);
-						stringTag = new StringTag(name, set);
-					}
-				} else if (tag instanceof CompoundTag) {
-					// System.out.println("(CompoundTag) enter: " + tag.getName());
-					CompoundTag ct = (CompoundTag) tag;
-					tag = writeNewTag(ct, name, replace, set);
-				} else if (tag instanceof ListTag) {
-					// System.out.println("(ListTag) enter: " + tag.getName());
-					tag = writeNewTag((ListTag) tag, name, replace, set);
-				}
-			}
+			tag = stream.readTag();
+			if (tag instanceof StringTag) {
+				StringTag stringTag = (StringTag) tag;
+				if (stringTag.getName().equals(name) && stringTag.getValue().equals(replace))
+					System.out.println("(StringTag) Replacing: " + replace + " with: " + set);
+			} else if (tag instanceof CompoundTag) {
+				CompoundTag ct = (CompoundTag) tag;
+				tag = writeNewTag(ct, name, replace, set);
+			} else if (tag instanceof ListTag)
+				tag = writeNewTag((ListTag) tag, name, replace, set);
 			stream.close();
 			NBTOutputStream out = new NBTOutputStream(new FileOutputStream(file));
 			out.writeTag(tag);
 			out.close();
 		}
-
 	}
 
-	static CompoundTag writeNewTag(CompoundTag ct, String name, String replace, String set) {
+	private static CompoundTag writeNewTag(CompoundTag ct, String name, String replace, String set) {
 		Iterator<Entry<String, Tag>> iter = ct.getValue().entrySet().iterator();
-		Map<String, Tag> map = new HashMap<String, Tag>();
+		Map<String, Tag> map = new HashMap<>();
 		while (iter.hasNext()) {
 			Entry<String, Tag> entry = iter.next();
 			Tag tag = entry.getValue();
@@ -109,20 +84,17 @@ public class NBTStringConvert {
 					System.out.println("(StringTag) Replacing: " + replace + " with: " + set);
 					tag = new StringTag(name, set);
 				}
-			} else if (tag instanceof CompoundTag) {
-				// System.out.println("(CompoundTag) enter: " + tag.getName());
+			} else if (tag instanceof CompoundTag)
 				tag = writeNewTag((CompoundTag) tag, name, replace, set);
-			} else if (tag instanceof ListTag) {
-				// System.out.println("(ListTag) enter: " + tag.getName());
+			else if (tag instanceof ListTag)
 				tag = writeNewTag((ListTag) tag, name, replace, set);
-			}
 			map.put(entry.getKey(), tag);
 		}
 		return new CompoundTag(ct.getName(), map);
 	}
 
-	static ListTag writeNewTag(ListTag lt, String name, String replace, String set) {
-		List<Tag> list = new ArrayList<Tag>();
+	private static ListTag writeNewTag(ListTag lt, String name, String replace, String set) {
+		List<Tag> list = new ArrayList<>();
 		for (Tag tag : lt.getValue()) {
 			if (tag instanceof StringTag) {
 				StringTag stringTag = ((StringTag) tag);
@@ -130,13 +102,10 @@ public class NBTStringConvert {
 					System.out.println("(StringTag) Replacing: " + replace + " with: " + set);
 					tag = new StringTag(name, set);
 				}
-			} else if (tag instanceof CompoundTag) {
-				// System.out.println("(CompoundTag) enter: " + tag.getName());
+			} else if (tag instanceof CompoundTag)
 				tag = writeNewTag((CompoundTag) tag, name, replace, set);
-			} else if (tag instanceof ListTag) {
-				// System.out.println("(ListTag) enter: " + tag.getName());
+			else if (tag instanceof ListTag)
 				tag = writeNewTag((ListTag) tag, name, replace, set);
-			}
 			list.add(tag);
 		}
 		return new ListTag(lt.getName(), lt.getType(), list);
